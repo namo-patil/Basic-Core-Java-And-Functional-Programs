86389ac5cb9a412130ebfe42b0477892c521d894
namo-patil <namratapatil74738@gmail.com>
2022-04-26 23:38:01 +0530

    Added: Program to prints the table of power of 2

---
+++ src/com/bridgelabz/basiccorejava/PowerOfTwo.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.basiccorejava;
+
+public class PowerOfTwo {
+    private static void printTableOfPowerTwo() {
+        int base = 2;
+        int N = 31;
+        for (int i = 0; i < N; i++) {
+            System.out.println("Power of " + base + "^" + i + " is : " + Math.pow(base, i));
+        }
+    }
+
+    public static void main(String[] args) {
+        printTableOfPowerTwo();
+    }
+}
